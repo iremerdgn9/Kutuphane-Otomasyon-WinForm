89d1680b4fecf0f9d98d5ef6667d548c256b1ab4
iremerdgn9 <irmerdgn06@gmail.com>
2022-12-23 19:19:26 +0300

    işlem paneli oluşturuldu

---
+++ IslemPaneli.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Kutuphane_Otomasyon_WinForm
+{
+    partial class IslemPaneli
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IslemPaneli));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ekleKullanicibtn = new System.Windows.Forms.Button();
+            this.silKullanicibtn = new System.Windows.Forms.Button();
+            this.guncelleKullanicibtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.guncelleKullanicibtn);
+            this.panel1.Controls.Add(this.silKullanicibtn);
+            this.panel1.Controls.Add(this.ekleKullanicibtn);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 719);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightCoral;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1120, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightPink;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 120);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kullanıcılar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ekleKullanicibtn
+            // 
+            this.ekleKullanicibtn.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ekleKullanicibtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ekleKullanicibtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ekleKullanicibtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ekleKullanicibtn.Location = new System.Drawing.Point(0, 120);
+            this.ekleKullanicibtn.Name = "ekleKullanicibtn";
+            this.ekleKullanicibtn.Size = new System.Drawing.Size(200, 91);
+            this.ekleKullanicibtn.TabIndex = 1;
+            this.ekleKullanicibtn.Text = "Ekle";
+            this.ekleKullanicibtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ekleKullanicibtn.UseVisualStyleBackColor = false;
+            this.ekleKullanicibtn.Click += new System.EventHandler(this.ekleKullanicibtn_Click);
+            // 
+            // silKullanicibtn
+            // 
+            this.silKullanicibtn.BackColor = System.Drawing.Color.LavenderBlush;
+            this.silKullanicibtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.silKullanicibtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.silKullanicibtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.silKullanicibtn.Location = new System.Drawing.Point(0, 211);
+            this.silKullanicibtn.Name = "silKullanicibtn";
+            this.silKullanicibtn.Size = new System.Drawing.Size(200, 91);
+            this.silKullanicibtn.TabIndex = 2;
+            this.silKullanicibtn.Text = "Sil";
+            this.silKullanicibtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.silKullanicibtn.UseVisualStyleBackColor = false;
+            // 
+            // guncelleKullanicibtn
+            // 
+            this.guncelleKullanicibtn.BackColor = System.Drawing.Color.LavenderBlush;
+            this.guncelleKullanicibtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guncelleKullanicibtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.guncelleKullanicibtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guncelleKullanicibtn.Location = new System.Drawing.Point(0, 302);
+            this.guncelleKullanicibtn.Name = "guncelleKullanicibtn";
+            this.guncelleKullanicibtn.Size = new System.Drawing.Size(200, 91);
+            this.guncelleKullanicibtn.TabIndex = 3;
+            this.guncelleKullanicibtn.Text = "Güncelle";
+            this.guncelleKullanicibtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.guncelleKullanicibtn.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(637, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KÜTÜPHANEMİZE HOŞGELDİNİZ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 141);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 232);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(12, 320);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(305, 105);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(913, 614);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // IslemPaneli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 719);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "IslemPaneli";
+            this.Text = "IslemPaneli";
+            this.Load += new System.EventHandler(this.IslemPaneli_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button guncelleKullanicibtn;
+        private System.Windows.Forms.Button silKullanicibtn;
+        private System.Windows.Forms.Button ekleKullanicibtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
---
+++ IslemPaneli.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kutuphane_Otomasyon_WinForm
+{
+    public partial class IslemPaneli : Form
+    {
+        public IslemPaneli()
+        {
+            InitializeComponent();
+        }
+        KutuphaneOtomasyonEntities db = new KutuphaneOtomasyonEntities();
+        private void IslemPaneli_Load(object sender, EventArgs e)
+        {
+            ekleKullanicibtn.Visible = false; //ekle butonu kapalı
+            silKullanicibtn.Visible = false; //sil butonu kapalı
+            guncelleKullanicibtn.Visible = false;//güncelle butonu kapalı olsun demektir.
+            label1.Visible = true;
+            pictureBox1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ekleKullanicibtn.Visible == false)
+            {
+                ekleKullanicibtn.Visible = true; //kullanıcılar butonuna basılınca açılacak demektir.
+                silKullanicibtn.Visible = true;
+                guncelleKullanicibtn.Visible = true;
+                label1.Visible = false;
+                pictureBox1.Visible = false;
+            }
+            else
+            {
+                ekleKullanicibtn.Visible = false; //kullanıcı butonu açıksa kapanır yani basılmazsa kapalı konumda olacaklar.
+                silKullanicibtn.Visible = false;
+                guncelleKullanicibtn.Visible = false;
+                label1.Visible = true;
+                pictureBox1.Visible = true;
+            }
+            KullaniciListeForm kListeForm = new KullaniciListeForm();
+            kListeForm.MdiParent = this; //bunun parent'i IslemPaneli'idir.
+            kListeForm.Show();
+        }
+
+        private void ekleKullanicibtn_Click(object sender, EventArgs e)
+        {
+            Kullanici.KullaniciEkleForm ekleForm = new Kullanici.KullaniciEkleForm();
+            ekleForm.MdiParent = this;
+            ekleForm.Show();
+        }
+    }
+}
