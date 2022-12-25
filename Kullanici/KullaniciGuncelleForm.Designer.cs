cffbccb4ce65bee8dcb02ff257dd40de29ae46d4
iremerdgn9 <irmerdgn06@gmail.com>
2022-12-26 01:58:42 +0300

    Kullanıcı Güncelle formu oluşturuldu

---
+++ Kullanici/KullaniciGuncelleForm.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace Kutuphane_Otomasyon_WinForm.Kullanici
+{
+    partial class KullaniciGuncelleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioK = new System.Windows.Forms.RadioButton();
+            this.radioE = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.kullaniciCezatxt = new System.Windows.Forms.TextBox();
+            this.kullaniciTeltxt = new System.Windows.Forms.TextBox();
+            this.kullaniciMailtxt = new System.Windows.Forms.TextBox();
+            this.kullaniciTctxt = new System.Windows.Forms.TextBox();
+            this.kullaniciSoyadtxt = new System.Windows.Forms.TextBox();
+            this.kullaniciAdtxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.Location = new System.Drawing.Point(84, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 49);
+            this.button1.TabIndex = 47;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // radioK
+            // 
+            this.radioK.AutoSize = true;
+            this.radioK.Location = new System.Drawing.Point(255, 229);
+            this.radioK.Name = "radioK";
+            this.radioK.Size = new System.Drawing.Size(74, 24);
+            this.radioK.TabIndex = 46;
+            this.radioK.TabStop = true;
+            this.radioK.Text = "Kadın";
+            this.radioK.UseVisualStyleBackColor = true;
+            // 
+            // radioE
+            // 
+            this.radioE.AutoSize = true;
+            this.radioE.Location = new System.Drawing.Point(174, 229);
+            this.radioE.Name = "radioE";
+            this.radioE.Size = new System.Drawing.Size(75, 24);
+            this.radioE.TabIndex = 45;
+            this.radioE.TabStop = true;
+            this.radioE.Text = "Erkek";
+            this.radioE.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(80, 229);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 20);
+            this.label7.TabIndex = 44;
+            this.label7.Text = "Cinsiyet:";
+            // 
+            // kullaniciCezatxt
+            // 
+            this.kullaniciCezatxt.Location = new System.Drawing.Point(174, 380);
+            this.kullaniciCezatxt.Name = "kullaniciCezatxt";
+            this.kullaniciCezatxt.Size = new System.Drawing.Size(192, 26);
+            this.kullaniciCezatxt.TabIndex = 43;
+            // 
+            // kullaniciTeltxt
+            // 
+            this.kullaniciTeltxt.Location = new System.Drawing.Point(174, 344);
+            this.kullaniciTeltxt.Name = "kullaniciTeltxt";
+            this.kullaniciTeltxt.Size = new System.Drawing.Size(192, 26);
+            this.kullaniciTeltxt.TabIndex = 42;
+            // 
+            // kullaniciMailtxt
+            // 
+            this.kullaniciMailtxt.Location = new System.Drawing.Point(174, 299);
+            this.kullaniciMailtxt.Name = "kullaniciMailtxt";
+            this.kullaniciMailtxt.Size = new System.Drawing.Size(192, 26);
+            this.kullaniciMailtxt.TabIndex = 41;
+            // 
+            // kullaniciTctxt
+            // 
+            this.kullaniciTctxt.Location = new System.Drawing.Point(174, 265);
+            this.kullaniciTctxt.Name = "kullaniciTctxt";
+            this.kullaniciTctxt.Size = new System.Drawing.Size(192, 26);
+            this.kullaniciTctxt.TabIndex = 40;
+            // 
+            // kullaniciSoyadtxt
+            // 
+            this.kullaniciSoyadtxt.Location = new System.Drawing.Point(174, 183);
+            this.kullaniciSoyadtxt.Name = "kullaniciSoyadtxt";
+            this.kullaniciSoyadtxt.Size = new System.Drawing.Size(192, 26);
+            this.kullaniciSoyadtxt.TabIndex = 39;
+            // 
+            // kullaniciAdtxt
+            // 
+            this.kullaniciAdtxt.Location = new System.Drawing.Point(174, 141);
+            this.kullaniciAdtxt.Name = "kullaniciAdtxt";
+            this.kullaniciAdtxt.Size = new System.Drawing.Size(192, 26);
+            this.kullaniciAdtxt.TabIndex = 38;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 386);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 20);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "Ceza:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 344);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 20);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Tel:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Mail:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "TC:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Soyad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Ad:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(447, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1086, 644);
+            this.dataGridView1.TabIndex = 48;
+            // 
+            // KullaniciGuncelleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(1560, 728);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioK);
+            this.Controls.Add(this.radioE);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.kullaniciCezatxt);
+            this.Controls.Add(this.kullaniciTeltxt);
+            this.Controls.Add(this.kullaniciMailtxt);
+            this.Controls.Add(this.kullaniciTctxt);
+            this.Controls.Add(this.kullaniciSoyadtxt);
+            this.Controls.Add(this.kullaniciAdtxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "KullaniciGuncelleForm";
+            this.Text = "KullaniciGuncelleForm";
+            this.Load += new System.EventHandler(this.KullaniciGuncelleForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioK;
+        private System.Windows.Forms.RadioButton radioE;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox kullaniciCezatxt;
+        private System.Windows.Forms.TextBox kullaniciTeltxt;
+        private System.Windows.Forms.TextBox kullaniciMailtxt;
+        private System.Windows.Forms.TextBox kullaniciTctxt;
+        private System.Windows.Forms.TextBox kullaniciSoyadtxt;
+        private System.Windows.Forms.TextBox kullaniciAdtxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Kullanici/KullaniciGuncelleForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kutuphane_Otomasyon_WinForm.Kullanici
+{
+    public partial class KullaniciGuncelleForm : Form
+    {
+        public KullaniciGuncelleForm()
+        {
+            InitializeComponent();
+        } 
+        KutuphaneOtomasyonEntities db = new KutuphaneOtomasyonEntities();
+
+        public void Listele() //metot oluşturduk
+        {
+            var kullanicilar = db.Kullanicilar.ToList(); //kullanıcıların hepsini buraya listele
+            dataGridView1.DataSource = kullanicilar.ToList();
+        }
+        private void KullaniciGuncelleForm_Load(object sender, EventArgs e)
+        {
+            Listele();
+        }
+    }
+}
