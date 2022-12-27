f74a2d43e3011caf783433ee69d29be84f142160
iremerdgn9 <irmerdgn06@gmail.com>
2022-12-27 11:45:41 +0300

    Kaynaklar adında database tableı oluşturduk ve projemizde update db yaptık

--- Model1.Context.cs
+++ Model1.Context.cs
@@ -27,5 +27,6 @@ namespace Kutuphane_Otomasyon_WinForm
     
         public virtual DbSet<Kullanicilar> Kullanicilar { get; set; }
         public virtual DbSet<Personeller> Personeller { get; set; }
+        public virtual DbSet<Kaynaklar> Kaynaklar { get; set; }
     }
 }
