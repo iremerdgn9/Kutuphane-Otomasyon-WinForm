fd15eb6259c503893986789a7833a6004c04bec8
iremerdgn9 <irmerdgn06@gmail.com>
2023-01-01 19:10:30 +0300

    Geri Al butonu oluşturduk

--- Kullanici/KullaniciSilForm.Designer.cs
+++ Kullanici/KullaniciSilForm.Designer.cs
@@ -37,18 +37,18 @@ namespace Kutuphane_Otomasyon_WinForm
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(5, 130);
+            this.dataGridView1.Location = new System.Drawing.Point(80, 131);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1333, 547);
+            this.dataGridView1.Size = new System.Drawing.Size(1266, 547);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.OrangeRed;
             this.button1.Font = new System.Drawing.Font("Modern No. 20", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(80, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(175, 90);
             this.button1.TabIndex = 1;
