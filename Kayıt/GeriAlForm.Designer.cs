9bd7a8fb61def2fa1d32a45a1b537ef4a09e733c
iremerdgn9 <irmerdgn06@gmail.com>
2023-01-01 19:13:12 +0300

    Forma ödünç durumu false olanları listeledik ve Geri Al butonu ile durumları true yaptık

---
+++ Kayıt/GeriAlForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Kutuphane_Otomasyon_WinForm.Kayıt
+{
+    partial class GeriAlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1016, 496);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(96, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 90);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Geri Al";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GeriAlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(1608, 697);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "GeriAlForm";
+            this.Text = "GeriAlForm";
+            this.Load += new System.EventHandler(this.GeriAlForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Kayıt/GeriAlForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kutuphane_Otomasyon_WinForm.Kayıt
+{
+    public partial class GeriAlForm : Form
+    {
+        public GeriAlForm()
+        {
+            InitializeComponent();
+        }
+        KutuphaneOtomasyonEntities db = new KutuphaneOtomasyonEntities();
+
+        private void GeriAlForm_Load(object sender, EventArgs e)
+        {
+            var kayitlar = db.Kayitlar.Where(x => x.durum == false).ToList(); //Kayıtlar tablosundaki durumu false olanları(geri getirmeyenleri) listele
+            dataGridView1.DataSource = kayitlar.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //geri al butonu
+        {
+            int secilenKayitId = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            var kayit = db.Kayitlar.Where(x => x.kayıt_id == secilenKayitId).FirstOrDefault(); //kayıt id'si seçilen kayıt id olanı bul
+            kayit.durum = true;
+            //db.SaveChanges(); savechange burda olursa geri al butonuna basınca direk true yapıp listeden çıkarır.
+
+            //liste tazele
+            var kayitlar = db.Kayitlar.Where(x => x.durum == false).ToList(); //Kayıtlar tablosundaki durumu false olanları(geri getirmeyenleri) listele
+            dataGridView1.DataSource = kayitlar.ToList(); //durumu false olanlar kitaplarını geri verdiğinde true oluyor
+            db.SaveChanges();
+
+        }
+    }
+}
